503cdabd724cea4f869ef1833d9fb8466eee8f1d
niyonsabaerina <niyonsabaerina1@gmail.com>
2025-02-21 10:31:34 +0200

    Add files via upload
    •	#include <iostream>: This is a standard library for input and output.
    •	int main(): This is the main function where execution starts.
    •	std::cout << "Hello, World!" << std::endl;: Prints text to the console.
    •	return 0; Indicates the program executed successfully.
    •	Required for using cout (for printing) and cin (for input).
    Primitive Data Types
    These are the fundamental built-in data types.
    Data Type	Size (Typical)	Description	Example
    int	4 bytes	Stores integers (whole numbers)	int x = 10;
    float	4 bytes	Stores decimal numbers (single precision)	float pi = 3.14f;
    double	8 bytes	Stores decimal numbers (double precision)	double pi = 3.1415926535;
    char	1 byte	Stores a single character	char letter = 'A';
    bool	1 byte	Stores true or false	bool isReady = true;
    void	No size	Represents no value (used in functions)	void function();
    Arithmetic Operators
    Used for mathematical operations.
    Operator	Description	Example
    +	Addition	a + b
    %	Modulus (Remainder)	 a %b
    -	Subtraction	a - b
    *	Multiplication	a * b
    /	Division	a / b

---
+++ OOPS/work3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	string name;
+    int age;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Hello, " << name << "! You are " << age << " years old." << endl;
+
+
+	
+	return 0;
+}
---
+++ OOPS/work4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a = 10, b = 3;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
+
+	return 0;
+}
